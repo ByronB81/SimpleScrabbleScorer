b6f1adc7f22edc0bef5bd31e8f2997cc8934a686
Byron <byronblocker@gmail.com>
2017-05-24 18:38:50 -0700

    almost too easy... now I gotta get back to my ship

--- Score.java
+++ Score.java
@@ -1,7 +1,45 @@
+import java.io.Console;
+
 public class Score {
   public static void main(String[] args){
+    Console console = System.console();
+    String[] wordArray;
+    int score = 0;
 
     System.out.println("Let's get started");
+    String userWord = console.readLine();
+    String lowerUserWord = userWord.toLowerCase();
+
+    wordArray = lowerUserWord.split("");
+
+
+    for (String letter : wordArray){
+      if (letter.matches(".*[aeioulnrst].*")){
+        score += 1;
+        System.out.println(letter.toUpperCase() + " is worth 1 point");
+      } else if (letter.matches(".*[dg].*")){
+        score += 2;
+        System.out.println(letter.toUpperCase() + " is worth 2 points");
+      } else if (letter.matches(".*[bcmp].*")){
+        score += 3;
+        System.out.println(letter.toUpperCase() + " is worth 3 points");
+      } else if (letter.matches(".*[fhvwy].*")){
+        score += 4;
+        System.out.println(letter.toUpperCase() + " is worth 4 points");
+      } else if (letter.matches(".*[k].*")) {
+        score += 5;
+        System.out.println(letter.toUpperCase() + " is worth 5 points");
+      } else if (letter.matches(".*[jx].*")){
+        score += 8;
+        System.out.println(letter.toUpperCase() + " is worth 8 points");
+      } else if (letter.matches(".*[qz].*")){
+        score += 10;
+        System.out.println(letter.toUpperCase() + " is worth 10 points");
+      }
+
+    }
+
+    System.out.println(userWord + " is worth " + score + " points!");
 
   }
 }
